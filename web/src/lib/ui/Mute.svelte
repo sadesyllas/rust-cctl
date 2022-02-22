32e95f8f4de5be8b320e2a4a5f9ed46596dc6654
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2022-02-22 10:31:09 +0200

    Fix muted/unmuted icon

--- web/src/lib/ui/Mute.svelte
+++ web/src/lib/ui/Mute.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { mdiVolumeOff } from '@mdi/js';
+  import { mdiVolumeHigh, mdiVolumeOff } from '@mdi/js';
 
   export let muted: boolean;
 </script>
 
 <div class="block" on:click>
   <svg viewBox="0 0 24 24" width="50" height="50" class:muted>
-    <path d={mdiVolumeOff} />
+    <path d={muted ? mdiVolumeOff : mdiVolumeHigh} />
   </svg>
 </div>
 
